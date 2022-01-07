de70fef350427a92fda42430646a6ab4a1c9ca0b
Beni Djokovic <56236600+arben-djokovic@users.noreply.github.com>
2022-01-07 13:46:03 +0100

    Update GameOver.js

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 
 export default function GameOver() {
-    var navigate = useNavigate()
+    const navigate = useNavigate()
     return (
         <div className='gameOverPage'>
             <h1>Game over</h1>
