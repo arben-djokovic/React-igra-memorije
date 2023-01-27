7ec1018c7609fef0e6dabe89131687917cd6f099
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2023-01-27 20:46:36 +0100

    Fixed errors

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState,useRef } from 'react'
 import { useNavigate } from 'react-router'
 
 export default function Game() {
-    var lista2 = []
-    var otvoreno = []
-    var lista1 = []
-    var navigate = useNavigate()
-    var numberFinished = 0
-    var pokusaji = 14
-    var pokusajiDiv = useRef()
-    var [list, setList] = useState([])
+    let lista2 = []
+    let otvoreno = []
+    let lista1 = []
+    let numberFinished = 0
+    let pokusaji = 12
+    let [list, setList] = useState([])
+    let pokusajiRef = useRef()
+    const navigate = useNavigate()
+
     do {
         let random = Math.floor(Math.random() * 10)
         if (!lista2.includes(random) && random <= 5) {
@@ -25,83 +26,70 @@ export default function Game() {
     } while (lista1.length !== 6)
 
 
-    var lista3 = [...lista1, ...lista2]
-    var lista4 = lista3
+    let lista3 = [...lista1, ...lista2]
+    let lista4 = lista3
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value)
+
+        let checkEnd = () => {
+            if (numberFinished === 6) {
+                lista1 = []
+                otvoreno = []
+                lista1 = []
+                lista3 = []
+                lista4 = []
+                numberFinished = 0
+                setList([])
+                navigate('/game-finished')
+            }
+            else if(pokusaji == 0){
+                navigate("/game-over")
+            }
+            else if(pokusaji * 2 < (12 - numberFinished*2)){
+                alert(`nemoguce zavrsiti: 
+                preostalo pokusaja: ${pokusaji},
+                preostalo duplih slika: ${(12 - numberFinished*2)/2}
+                `)
+                navigate("/game-over")
+            }
+        }
+
     useEffect(() => {
         setList(lista4)
     }, [])
     return (
         <div className='gamePage'>
-            <h1 className='pokusaji'>Preostalo pokusaja: <span ref={pokusajiDiv}>{pokusaji}</span> </h1>
+            <h1 className='pokusaji'>Preostalo pokusaja: <span ref={pokusajiRef}>{pokusaji}</span> </h1>
             <div className="gameGrid">
                 {list.map(a => {
                     return (<div key={Math.random()} onClick={(e) => {
-                        console.log()
                         if (e.target.style.opacity != 1 && e.target.className != 'imgDiv') {
-                            if (otvoreno.length !== 2) {
-                                e.target.style.cssText += 'opacity: 1'
-                                otvoreno = [...otvoreno, e.target.className]
-                                if (otvoreno.length === 2) {
-                                    pokusaji = pokusaji-1
-                                    pokusajiDiv.current.innerText = pokusaji
-                                    if(pokusaji === 0){
-                                        if(otvoreno[0] !== otvoreno[1]){
-                                            lista1 = []
-                                            otvoreno = []
-                                            lista1 = []
-                                            lista3 = []
-                                            lista4 = []
-                                            numberFinished = 0
-                                            setList([])
-                                            navigate('/game-over')
-                                        }
-                                    }
-                                    if (otvoreno[0] !== otvoreno[1]) {
-                                    }
-                                    else {
-                                        numberFinished = numberFinished + 1
-                                        if (numberFinished === 6) {
-                                            lista1 = []
-                                            otvoreno = []
-                                            lista1 = []
-                                            lista3 = []
-                                            lista4 = []
-                                            numberFinished = 0
-                                            setList([])
-                                            navigate('/game-finished')
-                                        }
-                                        otvoreno = []
-                                    }
-                                }
-                            }
-                            else {
-                                if (otvoreno[0] !== otvoreno[1]) {
-                                    document.getElementsByClassName(otvoreno[0])[0].style.cssText += 'opacity: 0'
-                                    document.getElementsByClassName(otvoreno[0])[1].style.cssText += 'opacity: 0'
-                                    document.getElementsByClassName(otvoreno[1])[0].style.cssText += 'opacity: 0'
-                                    document.getElementsByClassName(otvoreno[1])[1].style.cssText += 'opacity: 0'
-                                    otvoreno = []
-                                }
-                                else {
+                            e.target.style.cssText += 'opacity: 1'
+                            if(otvoreno.length == 1){
+                                otvoreno = [otvoreno[0], e.target.className]
+                                if(otvoreno[0] == otvoreno[1]){
                                     numberFinished = numberFinished + 1
-                                    if (numberFinished === 6) {
-                                        lista1 = []
-                                        otvoreno = []
-                                        lista1 = []
-                                        lista3 = []
-                                        lista4 = []
-                                        numberFinished = 0
-                                        setList([])
-                                        navigate('game-over')
-                                    }
-                                    otvoreno = []
                                 }
+                                else{
+                                    let otvorenoFirst = otvoreno[0]
+                                    let otvorenoSecond = otvoreno[1]
+                                    setTimeout(() => {
+                                        document.getElementsByClassName(otvorenoFirst)[0].style.cssText += 'opacity: 0'
+                                        document.getElementsByClassName(otvorenoFirst)[1].style.cssText += 'opacity: 0'
+                                        document.getElementsByClassName(otvorenoSecond)[0].style.cssText += 'opacity: 0'
+                                        document.getElementsByClassName(otvorenoSecond)[1].style.cssText += 'opacity: 0'
+                                    }, 400);
+                                }
+                                pokusaji = pokusaji - 1
+                                pokusajiRef.current.innerHTML = pokusaji
+                                otvoreno = []
                             }
+                            else if(otvoreno.length == 0){
+                                otvoreno = [e.target.className]
+                            }
+                            checkEnd()
                         }
-
                     }} className='imgDiv'><img draggable="false" className={a} src={'./images/card-' + (a + 1) + '.jpeg'} /></div>)
                 })}
             </div>
--- src/components/GameFinished.js
+++ src/components/GameFinished.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 
 export default function GameFinished() {
-    var navigate = useNavigate()
+    const navigate = useNavigate()
     return (
         <div className='gameOverPage'>
             <h1>Good job!</h1>
